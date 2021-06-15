2b450bfaaed1bf3ce617ecf9cd4cfa3fde6bbf93
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-15 18:12:27 +0900

    ネットワークコールバッククラス

---
+++ Assets/Scripts/Network/NetworkCallback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Bolt;
+
+namespace Game.Network
+{
+    /// <summary>
+    /// ネットワークコールバック
+    /// </summary>
+    [BoltGlobalBehaviour]
+    public class NetworkCallback : GlobalEventListener
+    {
+        /// <summary>
+        /// シーンロードが完了した
+        /// </summary>
+        /// <param name="scene">シーン名</param>
+        /// <param name="token">プロトコルトークン</param>
+        public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+        {
+            Debug.Log("Scene:" + scene);
+        }
+    }
+}
