f7ebfb17cf2aa4f563b5226ac2d0cc63b9008ac7
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-15 18:16:52 +0900

    サーバ・クライアントそれぞれとして起動するためのボタンを配置

---
+++ Assets/Scripts/UI/ClientButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// クライアントボタン
+    /// </summary>
+    public class ClientButton : MonoBehaviour
+    {
+    }
+}
