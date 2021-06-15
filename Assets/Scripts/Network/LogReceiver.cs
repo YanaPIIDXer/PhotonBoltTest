4413720c080a71f58c9f6119bc5511880e0dc121
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-15 22:10:16 +0900

    ログ受信クラス ※Debug.Logで出力しているので実質Editorでしか機能しない

---
+++ Assets/Scripts/Network/LogReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Bolt;
+
+namespace Game.Network
+{
+    /// <summary>
+    /// ログ受信
+    /// </summary>
+    [BoltGlobalBehaviour]
+    public class LogReceiver : GlobalEventListener
+    {
+        /// <summary>
+        /// ログ受信
+        /// </summary>
+        /// <param name="log">ログ</param>
+        public override void OnEvent(LogEvent log)
+        {
+            Debug.Log(log.Message);
+        }
+    }
+}
